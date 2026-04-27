d17f7ea2ba3040fd1632f51cb20dca46a501e8cf
sndfsecurity <sndf4315@gmail.com>
2026-04-27 15:18:07 +0530

    Initial backend setup with Spring Boot

---
+++ sndf-backend/src/main/java/com/sndf/backend/SndfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sndf.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SndfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SndfBackendApplication.class, args);
+	}
+
+}
---
+++ sndf-backend/src/test/java/com/sndf/backend/SndfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sndf.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SndfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
