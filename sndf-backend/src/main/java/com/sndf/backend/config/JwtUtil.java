06637fb509290410868d5be597a8eda8e57497af
sndfsecurity <sndf4315@gmail.com>
2026-06-07 13:02:47 +0530

    changes updated

--- sndf-backend/src/main/java/com/sndf/backend/config/JwtUtil.java
+++ sndf-backend/src/main/java/com/sndf/backend/config/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.sndf.backend.config;
 
 import io.jsonwebtoken.Claims;
+
+
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
--- sndf-backend/src/main/java/com/sndf/backend/config/SecurityConfig.java
+++ sndf-backend/src/main/java/com/sndf/backend/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.sndf.backend.config;
 
 import java.util.Arrays;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
